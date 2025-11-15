00312938410c88e236407e7795b3b0ff7663f6f7
Kieran Rosinger <Kieran.rosinger@Aveva.com>
2025-11-15 08:56:24 +0000

    Testing physical stops

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1909,11 +1909,11 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+//#define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS 220
+#define Y_MAX_POS 220
 #define Z_MAX_POS 220
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
