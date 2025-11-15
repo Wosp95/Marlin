4a85ea372197afca254a3d8c78703607a97c911d
Kieran Rosinger <Kieran.rosinger@Aveva.com>
2025-11-15 08:58:40 +0000

    fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1909,7 +1909,7 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-//#define X_MIN_POS 0
+#define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS 220
